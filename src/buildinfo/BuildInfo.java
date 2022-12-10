ae4d84d239cc4f09d131b70c4f99e0094ff5322a
Administrator <pbull809@gmail.com>
2022-12-10 09:06:14 +0100

    scrollbar now can change video pos

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "254";
-    private static final String BUILD_DATE = "12/10/2022 03:20:35 AM";
+    private static final String BUILD_NUMBER = "262";
+    private static final String BUILD_DATE = "12/10/2022 08:58:13 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
---
+++ src/video/MediaPlayerBox.java
@@ -0,0 +1,88 @@
+package video;
+
+import database.DBHandler;
+import uk.co.caprica.vlcj.media.MediaRef;
+import uk.co.caprica.vlcj.media.TrackType;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventListener;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MediaPlayerBox {
+    private volatile JFrame playerFrame;
+    private EmbeddedMediaPlayerComponent mpc;
+    private final JScrollBar sbar;
+
+    public MediaPlayerBox() {
+        sbar = new JScrollBar(Scrollbar.HORIZONTAL);
+        sbar.setBackground(Color.YELLOW);
+        UIManager.getLookAndFeelDefaults().put( "ScrollBar.thumb", Color.blue );
+        sbar.setMaximum(1000);
+        sbar.addAdjustmentListener(new AdjustmentListener() {
+            @Override
+            public void adjustmentValueChanged(AdjustmentEvent adjustmentEvent) {
+                if (mpc == null)
+                    return;
+                if (adjustmentEvent.getValueIsAdjusting()) {
+                    var mp = mpc.mediaPlayer().controls();
+                    mp.pause();
+                    mp.setPosition(adjustmentEvent.getValue()/1000f);
+                    mp.play();
+                }
+            }
+        });
+    }
+
+
+    public void start(JList<DBHandler.NameID> listControl) {
+        if (playerFrame != null)
+            return;
+        String name = listControl.getSelectedValue().name;
+        try {
+            String tempFile = DBHandler.getInst().transferVideoIntoFile(name);
+            System.out.println(tempFile);
+            mpc = new EmbeddedMediaPlayerComponent();
+            playerFrame = new JFrame();
+            playerFrame.setTitle("Media Player Box");
+            playerFrame.setBounds(100, 100, 600, 400);
+            playerFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            playerFrame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    stop();
+                }
+            });
+            playerFrame.setLayout(new BorderLayout());
+            playerFrame.add (mpc, BorderLayout.CENTER); //setContentPane(mpc);
+            playerFrame.add (sbar, BorderLayout.SOUTH);
+            playerFrame.setVisible(true);
+            mpc.mediaPlayer().events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+                @Override
+                public void positionChanged(MediaPlayer mediaPlayer, float v) {
+                    sbar.setValue((int)(v*1000));
+                }
+            });
+            mpc.mediaPlayer().videoSurface().attachVideoSurface();
+            mpc.mediaPlayer().media().play(tempFile);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void stop() {
+        if (playerFrame == null)
+            return;
+        System.out.println("stop");
+        mpc.release();
+        playerFrame.dispose();
+        playerFrame = null;
+    }
+
+}
