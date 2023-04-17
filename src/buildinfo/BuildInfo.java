23c5eaf826126f4057f722de96ef2d024c2a0bed
Administrator <pbull809@gmail.com>
2023-04-17 19:31:00 +0200

    added web interface (webserver)

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "476";
-    private static final String BUILD_DATE = "04/17/2023 03:36:21 AM";
+    private static final String BUILD_NUMBER = "504";
+    private static final String BUILD_DATE = "04/17/2023 07:29:49 PM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
--- src/thegrid/WebApp.java
+++ src/thegrid/WebApp.java
@@ -14,13 +14,18 @@ import fi.iki.elonen.NanoHTTPD;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
+
+import static common.Tools.extractResource;
 
 public class WebApp extends NanoHTTPD {
     private final java.util.List<DBHandler.NameID> allFiles;
+    private final UniqueRng ring;
 
     public WebApp() {
         super(80);
         allFiles = DBHandler.getInst().getImageFileNames();
+        ring = new UniqueRng (allFiles.size());
 
         try {
             start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
@@ -35,8 +40,8 @@ public class WebApp extends NanoHTTPD {
         try {
             return newFixedLengthResponse(Response.Status.OK,
                     "image/jpeg", is, is.available());
-        } catch (IOException e) {
-            return null;
+        } catch (Exception e) {
+            return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/html", "Error");
         }
     }
 
@@ -44,8 +49,10 @@ public class WebApp extends NanoHTTPD {
     public Response serve(IHTTPSession session) {
         String uri = session.getUri();
         int rowid = 0;
+        String nude = "Empty";
         try {
-            rowid = Integer.parseInt(uri.substring(1, uri.length() - 4));
+            nude = uri.substring(1, uri.length() - 4);
+            rowid = Integer.parseInt(nude);
         } catch (Exception e) {
             //throw new RuntimeException(e);
         }
@@ -64,8 +71,42 @@ public class WebApp extends NanoHTTPD {
             byte[] bytes = DBHandler.getInst().loadThumbnail(rowid);
             return sendImageBytes(bytes);
         } else if (uri.endsWith(".lnk")) {
+            try {
+                String str = new String (extractResource("imgpage0.html"));
+                str = str.replace ("@@THEIMG", ""+rowid+".pix");
+//                str = str.replace ("@@PRV", ""+allFiles.get(ring.getPrev()).rowid);
+//                str = str.replace ("@@NXT", ""+allFiles.get(ring.getNext()).rowid);
+                return newFixedLengthResponse (str);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else if (uri.endsWith(".pix")) {
             byte[] bytes = DBHandler.getInst().loadImage(rowid);
             return sendImageBytes(bytes);
+        } else if (uri.endsWith(".ico")) {
+            byte[] bytes = new byte[0];
+            try {
+                bytes = extractResource(uri.substring(1));
+                return sendImageBytes(bytes);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else if (uri.startsWith("/show.html")) {
+            Map<String, String> m = session.getParms();
+            String parm = m.get("img");
+            if (parm.equals("@@PRV")) {
+                rowid = allFiles.get(ring.getPrev()).rowid;
+            } else {
+                rowid = allFiles.get(ring.getNext()).rowid;
+            }
+            try {
+                String str = new String (extractResource("imgpage0.html"));
+                str = str.replace ("@@THEIMG", ""+rowid+".pix");
+                return newFixedLengthResponse (str);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
         }
         return null;
     }
