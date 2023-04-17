dc6d28989f30e040b86afe3052b313a4f3ce8f81
Administrator <pbull809@gmail.com>
2023-04-18 00:12:42 +0200

    some improvements & new jarfile

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "504";
-    private static final String BUILD_DATE = "04/17/2023 07:29:49 PM";
+    private static final String BUILD_NUMBER = "509";
+    private static final String BUILD_DATE = "04/18/2023 12:08:10 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
--- src/thegrid/WebApp.java
+++ src/thegrid/WebApp.java
@@ -45,11 +45,55 @@ public class WebApp extends NanoHTTPD {
         }
     }
 
+    private Response sendImagePage (int rowid) {
+        try {
+            String str = new String (extractResource("imgpage0.html"));
+            str = str.replace ("@@THEIMG", rowid+".pix");
+            return newFixedLengthResponse (str);
+        } catch (Exception e) {
+            return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/html", "Error");
+        }
+    }
+
+    private Response sendDirectory() {
+        StringBuilder msg = new StringBuilder("<html><body>\n");
+        for (DBHandler.NameID nid : allFiles) {
+            msg.append("<a href=\"")
+                    .append(nid.rowid)
+                    .append(".lnk\" target=\"_blank\"><img src=\"")
+                    .append(nid.rowid)
+                    .append (".jpg\" width=\"50\" height=\"50\"></a>\n");
+        }
+        msg.append("</body></html>\n");
+        return newFixedLengthResponse(msg.toString());
+    }
+
+    private Response sendIcon(String uri) {
+        byte[] bytes;
+        try {
+            bytes = extractResource(uri.substring(1));
+            return sendImageBytes(bytes);
+        } catch (Exception e) {
+            return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/html", "Error");
+        }
+    }
+
+    private Response switchImage(IHTTPSession session) {
+        int rowid;
+        String parm = session.getParms().get("img");
+        if (parm.equals("@@PRV")) {
+            rowid = allFiles.get(ring.getPrev()).rowid;
+        } else {
+            rowid = allFiles.get(ring.getNext()).rowid;
+        }
+        return sendImagePage (rowid);
+    }
+
     @Override
     public Response serve(IHTTPSession session) {
         String uri = session.getUri();
         int rowid = 0;
-        String nude = "Empty";
+        String nude;
         try {
             nude = uri.substring(1, uri.length() - 4);
             rowid = Integer.parseInt(nude);
@@ -57,56 +101,19 @@ public class WebApp extends NanoHTTPD {
             //throw new RuntimeException(e);
         }
         if (uri.equals("/")) {
-            StringBuffer msg = new StringBuffer ("<html><body>\n");
-            for (DBHandler.NameID nid : allFiles) {
-                msg.append("<a href=\"")
-                        .append(nid.rowid)
-                        .append(".lnk\"><img src=\"")
-                        .append(nid.rowid)
-                        .append (".jpg\" width=\"50\" height=\"50\"></a>\n");
-            }
-            msg.append("</body></html>\n");
-            return newFixedLengthResponse(msg.toString());
+            return sendDirectory();
         } else if (uri.endsWith(".jpg")) {
             byte[] bytes = DBHandler.getInst().loadThumbnail(rowid);
             return sendImageBytes(bytes);
         } else if (uri.endsWith(".lnk")) {
-            try {
-                String str = new String (extractResource("imgpage0.html"));
-                str = str.replace ("@@THEIMG", ""+rowid+".pix");
-//                str = str.replace ("@@PRV", ""+allFiles.get(ring.getPrev()).rowid);
-//                str = str.replace ("@@NXT", ""+allFiles.get(ring.getNext()).rowid);
-                return newFixedLengthResponse (str);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            return sendImagePage (rowid);
         } else if (uri.endsWith(".pix")) {
             byte[] bytes = DBHandler.getInst().loadImage(rowid);
             return sendImageBytes(bytes);
         } else if (uri.endsWith(".ico")) {
-            byte[] bytes = new byte[0];
-            try {
-                bytes = extractResource(uri.substring(1));
-                return sendImageBytes(bytes);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            return sendIcon(uri);
         } else if (uri.startsWith("/show.html")) {
-            Map<String, String> m = session.getParms();
-            String parm = m.get("img");
-            if (parm.equals("@@PRV")) {
-                rowid = allFiles.get(ring.getPrev()).rowid;
-            } else {
-                rowid = allFiles.get(ring.getNext()).rowid;
-            }
-            try {
-                String str = new String (extractResource("imgpage0.html"));
-                str = str.replace ("@@THEIMG", ""+rowid+".pix");
-                return newFixedLengthResponse (str);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
+            return switchImage(session);
         }
         return null;
     }
