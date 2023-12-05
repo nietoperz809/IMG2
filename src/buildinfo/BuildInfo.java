8bf01882a36048382d0983419e9a8b48cabf0591
Administrator <pbull809@gmail.com>
2023-12-05 07:13:13 +0100

    some little changes

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "1253";
-    private static final String BUILD_DATE = "12/05/2023 07:05:43 AM";
+    private static final String BUILD_NUMBER = "1254";
+    private static final String BUILD_DATE = "12/05/2023 07:12:15 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
