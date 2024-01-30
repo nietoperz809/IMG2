79f4c19e0f8fd8c5a63c0b254d6928fe1bc2a3a2
Administrator <pbull809@gmail.com>
2024-01-30 22:52:23 +0100

    introduced ring2

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "1313";
-    private static final String BUILD_DATE = "01/30/2024 10:47:37 PM";
+    private static final String BUILD_NUMBER = "1314";
+    private static final String BUILD_DATE = "01/30/2024 10:51:21 PM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
