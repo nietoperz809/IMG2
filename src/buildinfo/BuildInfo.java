3db08fcab699a50f11f69cb79224086aa01c024a
Administrator <pbull809@gmail.com>
2024-10-25 06:55:35 +0200

    VideoApp dragdrop extended for multiple files

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "1716";
-    private static final String BUILD_DATE = "10/25/2024 06:51:08 AM";
+    private static final String BUILD_NUMBER = "1718";
+    private static final String BUILD_DATE = "10/25/2024 06:54:44 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
