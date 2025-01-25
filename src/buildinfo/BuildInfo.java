527b4dadcc327014afbf6c5e0f281374e5a948ca
Administrator <pbull809@gmail.com>
2025-01-25 10:42:35 +0100

    bugfix in searchdupes

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "2260";
-    private static final String BUILD_DATE = "01/25/2025 06:37:03 AM";
+    private static final String BUILD_NUMBER = "2266";
+    private static final String BUILD_DATE = "01/25/2025 10:41:18 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
--- src/thegrid/GridMenuBar.java
+++ src/thegrid/GridMenuBar.java
@@ -4,6 +4,7 @@ import common.*;
 import database.DBHandler;
 import dialogs.*;
 import httpserv.WebApp;
+import org.jetbrains.annotations.NotNull;
 import video.VideoApp;
 
 import javax.swing.*;
@@ -198,12 +199,8 @@ public class GridMenuBar extends JMenuBar {
         });
         jm.add(jmi);
 
-        jmi = searchDupes ("Search for double Items");
+        jmi = searchDupes("Search for duplicates");
         jm.add(jmi);
-/*
-        jmi = searchDupes(true, theGrid, "Delete double Items");
-        jm.add(jmi);
-*/
 
         jmi = new JMenuItem("video App");
         jmi.addActionListener(new AbstractAction() {
@@ -406,17 +403,18 @@ public class GridMenuBar extends JMenuBar {
 [11451, 11453]
      */
 
-    private JMenuItem searchDupes (String text) {
+    private @NotNull JMenuItem searchDupes(String text) {
         JMenuItem m3 = new JMenuItem(text);
         m3.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ArrayList<byte[]> map = DBHandler.loadImgHashes();
                 ArrayList<Integer> found = new ArrayList<>();
-                //StringBuilder output = new StringBuilder();
-                for (int s=0; s<map.size(); s++) {
-                    for (int n=s+1; n<map.size(); n++) {
-                        if (Arrays.equals (map.get(n), map.get(s))) {
+                for (int s = 0; s < map.size(); s++) {
+                    byte[] bs = map.get(s);
+                    for (int n = s + 1; n < map.size(); n++) {
+                        byte[] bn = map.get(n);
+                        if (Arrays.equals(bs, bn)) {
                             List<Integer> li = RowIDfromImgHash(map.get(s));
                             if (found.containsAll(li))
                                 continue;
@@ -424,15 +422,19 @@ public class GridMenuBar extends JMenuBar {
                         }
                     }
                 }
-                StringBuilder sqlFound = new StringBuilder();
-                sqlFound.append("select name,_ROWID_,tag,accnum from IMAGES where ");
-                for (int i: found) {
-                    sqlFound.append("_rowid_=").append(i).append(" or ");
+                // any found?
+                if (!found.isEmpty()) {
+
+                    StringBuilder sqlFound = new StringBuilder();
+                    sqlFound.append("select name,_ROWID_,tag,accnum from IMAGES where ");
+                    for (int i : found) {
+                        sqlFound.append("_rowid_=").append(i).append(" or ");
+                    }
+                    sqlFound.setLength(sqlFound.length() - 4);
+                    (new Thread(() -> {
+                        new TheGrid(sqlFound.toString(), "WORKER");
+                    })).start();
                 }
-                sqlFound.setLength (sqlFound.length()-4);
-                (new Thread(() -> {
-                    new TheGrid(sqlFound.toString(), "WORKER");
-                })).start();
                 //System.out.println(sqlFound.toString());
             }
         });
