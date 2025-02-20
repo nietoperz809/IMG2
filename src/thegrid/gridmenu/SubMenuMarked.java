3e7c0011f04fe61af1ffa81a569dd90fd50acbdc
Administrator <pbull809@gmail.com>
2025-02-20 06:38:09 +0100

    wrote AddItem func

--- src/thegrid/gridmenu/SubMenuMarked.java
+++ src/thegrid/gridmenu/SubMenuMarked.java
@@ -6,8 +6,6 @@ import common.Tools;
 import database.DBHandler;
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.model.enums.CompressionLevel;
-import net.lingala.zip4j.model.enums.EncryptionMethod;
 import thegrid.GridImage;
 import thegrid.TheGrid;
 
@@ -19,93 +17,99 @@ import java.io.IOException;
 
 public class SubMenuMarked extends JMenu {
 
+    private void addItem(String mtext, AbstractAction aa) {
+        JMenuItem jmi = new JMenuItem(mtext);
+        jmi.addActionListener(aa);
+        add (jmi);
+    }
+
     public SubMenuMarked (final TheGrid grid) {
         super ("Marked ...");
 
-        JMenuItem jmi;
-
-        jmi = new JMenuItem("Mark all");
-        jmi.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GridImage.markAll(grid, true);
-            }
-        });
-        add(jmi);
-
-        jmi = new JMenuItem("Unmark all");
-        jmi.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GridImage.markAll(grid, false);
-            }
-        });
-        add(jmi);
-
-        jmi = new JMenuItem("Toggle");
-        jmi.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GridImage.toggleMarks(grid);
-            }
-        });
-        add(jmi);
-
-        jmi = new JMenuItem("Delete from DB");
-        jmi.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GridImage[] marked = GridImage.getMarked(grid);
-                for (GridImage gi : marked) {
-                    int id = gi.getRowID();
-                    DBHandler.deleteImageSilently(id);
-                }
-            }
-        });
-        add(jmi);
-
-        jmi = new JMenuItem("Save to Disk");
-        jmi.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GridImage[] marked = GridImage.getMarked(grid);
-                String outPath = Tools.chooseDir(SubMenuMarked.this);
-                for (GridImage gi : marked) {
-                    int id = gi.getRowID();
-                    byte[] b = DBHandler.loadImage(id);
-                    BufferedImage b2 = ImgTools.byteArrayToImg(b);
-                    ImgTools.saveImg2Disk(b2, id, outPath);
-                }
-                GridImage.markAll(grid, false);
-            }
-        });
-        add(jmi);
-
-        jmi = new JMenuItem("Make Zip");
-        jmi.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ZipParameters zipParameters = Tools.getStandardZipParams();
-                String outPath = Tools.chooseDir(SubMenuMarked.this);
-                try {
-                    ZipFile zipFile = new ZipFile (outPath+ File.separator +
-                            System.currentTimeMillis()+"-images.rar",
-                            "imagebase".toCharArray());
-                    GridImage[] marked = GridImage.getMarked(grid);
-                    for (GridImage gi : marked) {
-                        int id = gi.getRowID();
-                        BufferedImage b2 = ImgTools.byteArrayToImg(DBHandler.loadImage(id));
-                        String imgFile = ImgTools.saveImg2Disk(b2, id, outPath);
-                        zipFile.addFile(imgFile,zipParameters);
-                        DeferredFileDeleter.put (new File(imgFile));
+        addItem("Mark all",
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        GridImage.markAll(grid, true);
                     }
-                    GridImage.markAll(grid, false);
-                    zipFile.close();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
-        add(jmi);
+                });
+
+        addItem("Unmark all",
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        GridImage.markAll(grid, false);
+                    }
+                });
+
+        addItem("Toggle",
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        GridImage.toggleMarks(grid);
+                    }
+                });
+
+        addItem("Toggle",
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        GridImage.toggleMarks(grid);
+                    }
+                });
+
+        addItem("Delete from DB",
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        GridImage[] marked = GridImage.getMarked(grid);
+                        for (GridImage gi : marked) {
+                            int id = gi.getRowID();
+                            DBHandler.deleteImageSilently(id);
+                        }
+                    }
+                });
+
+        addItem("Save to Disk",
+                new AbstractAction() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            GridImage[] marked = GridImage.getMarked(grid);
+                            String outPath = Tools.chooseDir(SubMenuMarked.this);
+                            for (GridImage gi : marked) {
+                                int id = gi.getRowID();
+                                byte[] b = DBHandler.loadImage(id);
+                                BufferedImage b2 = ImgTools.byteArrayToImg(b);
+                                ImgTools.saveImg2Disk(b2, id, outPath);
+                            }
+                            GridImage.markAll(grid, false);
+                        }
+                    });
+
+        addItem("Make Zip",
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        ZipParameters zipParameters = Tools.getStandardZipParams();
+                        String outPath = Tools.chooseDir(SubMenuMarked.this);
+                        try {
+                            ZipFile zipFile = new ZipFile (outPath+ File.separator +
+                                    System.currentTimeMillis()+"-images.rar",
+                                    "imagebase".toCharArray());
+                            GridImage[] marked = GridImage.getMarked(grid);
+                            for (GridImage gi : marked) {
+                                int id = gi.getRowID();
+                                BufferedImage b2 = ImgTools.byteArrayToImg(DBHandler.loadImage(id));
+                                String imgFile = ImgTools.saveImg2Disk(b2, id, outPath);
+                                zipFile.addFile(imgFile,zipParameters);
+                                DeferredFileDeleter.put (new File(imgFile));
+                            }
+                            GridImage.markAll(grid, false);
+                            zipFile.close();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                });
     }
 }
