7c8b2698c3d9328f25274f55ec96be16f849e9a3
Administrator <pbull809@gmail.com>
2025-03-04 00:51:23 +0100

    backup

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "2595";
-    private static final String BUILD_DATE = "03/03/2025 05:00:20 PM";
+    private static final String BUILD_NUMBER = "2598";
+    private static final String BUILD_DATE = "03/03/2025 08:46:51 PM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
