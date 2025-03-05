7c1fd3e88e96176291b7122497be44c73fa59e5c
Administrator <pbull809@gmail.com>
2025-03-05 07:13:24 +0100

    adding libs

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "2606";
-    private static final String BUILD_DATE = "03/04/2025 11:28:48 AM";
+    private static final String BUILD_NUMBER = "2612";
+    private static final String BUILD_DATE = "03/05/2025 05:56:59 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
