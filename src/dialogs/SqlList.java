cc0f289fe936527413cb9e8cb9c2679aa587bff6
Administrator <pbull809@gmail.com>
2025-03-14 05:28:43 +0100

    backup

--- src/dialogs/SqlList.java
+++ src/dialogs/SqlList.java
@@ -1,17 +1,34 @@
 package dialogs;
 
+import database.DBHandler;
+
 import javax.swing.*;
+import javax.swing.event.ListDataListener;
+import java.util.ArrayList;
 
 public class SqlList {
     private JPanel panel1;
     private JButton button1;
-    private JList list1;
+    private JList<String> list1;
+
+    public void populateList() {
+        ArrayList<String> al = DBHandler.getQueries();
+        DefaultListModel<String> lm = new DefaultListModel<>();
+        for (String s : al) {
+            lm.addElement(s);
+        }
+        list1.setModel(lm);
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SqlList");
-        frame.setContentPane(new SqlList().panel1);
+        SqlList sqll = new SqlList();
+        sqll.populateList();
+        frame.setContentPane(sqll.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+
         frame.setVisible(true);
     }
+
 }
