e23def40a3745ef7a7d9a1fea568883bf1073aa3
Administrator <pbull809@gmail.com>
2025-04-03 03:56:45 +0200

    backup

--- src/buildinfo/BuildInfo.java
+++ src/buildinfo/BuildInfo.java
@@ -1,8 +1,8 @@
 package buildinfo;
 
 public class BuildInfo {
-    private static final String BUILD_NUMBER = "2930";
-    private static final String BUILD_DATE = "04/02/2025 09:40:43 PM";
+    private static final String BUILD_NUMBER = "2939";
+    private static final String BUILD_DATE = "04/03/2025 03:52:34 AM";
 
     public static final String buildInfo = "ImageBase, Build: " + BUILD_NUMBER + " -- " + BUILD_DATE
             + " -- " + System.getProperty("java.version");
--- src/dialogs/RGBScroll.java
+++ src/dialogs/RGBScroll.java
@@ -9,8 +9,6 @@ import java.awt.image.BufferedImage;
 
 public class RGBScroll extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
     private JSlider sliderRed;
     private JSlider sliderGreen;
     private JSlider sliderBlue;
@@ -26,17 +24,13 @@ public class RGBScroll extends JDialog {
         setContentPane(contentPane);
         setModal(true);
         setUndecorated(true);
-        getRootPane().setDefaultButton(buttonOK);
 
         sliderRed.setMaximum(255);
-        sliderRed.setMinimum(0);
+        sliderRed.setMinimum(1);
         sliderGreen.setMaximum(255);
-        sliderGreen.setMinimum(0);
+        sliderGreen.setMinimum(1);
         sliderBlue.setMaximum(255);
-        sliderBlue.setMinimum(0);
-
-        buttonOK.addActionListener(e -> onOK());
-        buttonCancel.addActionListener(e -> onCancel());
+        sliderBlue.setMinimum(1);
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -47,37 +41,38 @@ public class RGBScroll extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
+        contentPane.registerKeyboardAction(e -> onCancel(),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        sliderRed.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                int v = sliderRed.getValue();
+                for (int s=0; s<image.getRGBData().length; s++)
+                    image.setRed (s, v);
+                panel.setImage(image);
             }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-
-        sliderRed.addChangeListener(e -> {
-            int v = sliderRed.getValue();
-            for (int s=0; s<image.getRGBData().length; s++)
-                image.setRed (s, (v+image.getRed(s))/2);
-            panel.setImage(image);
         });
 
-        sliderGreen.addChangeListener(e -> {
-            int v = sliderGreen.getValue();
-            for (int s=0; s<image.getRGBData().length; s++)
-                image.setGreen (s, (v+image.getGreen(s))/2);
-            panel.setImage(image);
+        sliderGreen.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                int v = sliderGreen.getValue();
+                for (int s=0; s<image.getRGBData().length; s++)
+                    image.setGreen (s, v);
+                panel.setImage(image);
+            }
         });
 
-        sliderBlue.addChangeListener(e -> {
-            int v = sliderBlue.getValue();
-            for (int s=0; s<image.getRGBData().length; s++)
-                image.setBlue (s, (v+image.getBlue(s))/2);
-            panel.setImage(image);
+        sliderBlue.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                int v = sliderBlue.getValue();
+                for (int s=0; s<image.getRGBData().length; s++)
+                    image.setBlue (s, v);
+                panel.setImage(image);
+            }
         });
-    }
-
-    private void onOK() {
-        // add your code here
-        dispose();
     }
 
     private void onCancel() {
