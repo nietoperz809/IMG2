47b3a294035b6ebf0e91d8fad06f52cc198fa3c0
Administrator <pbull809@gmail.com>
2025-04-06 10:15:38 +0200

    introduced cache

--- src/database/AccessCounter.java
+++ src/database/AccessCounter.java
@@ -2,10 +2,16 @@ package database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class AccessCounter extends DBHandler {
+    private static final HashMap<Integer, Integer> cache = new HashMap<>();
+
     public static synchronized void incAccCounter(int rowid) {
+        Integer val = cache.get(rowid);
+        if (val != null
+            cache.put(rowid, val+1);
         String sql = "update IMAGES set ACCNUM = (ACCNUM + 1) where _rowid_ =" + rowid;
         try {
             statement.execute(sql);
@@ -16,6 +22,7 @@ public class AccessCounter extends DBHandler {
     }
 
     public static synchronized void setAccCounter(int rowid, int val) {
+        cache.put(rowid, val);
         String sql = "update IMAGES set ACCNUM = " + val + " where _rowid_ =" + rowid;
         try {
             statement.execute(sql);
@@ -26,16 +33,20 @@ public class AccessCounter extends DBHandler {
     }
 
     public static synchronized int getAccCounter(int rowid) {
+        Integer ret = cache.get(rowid);
+        if (ret != null)
+            return ret;
         String q = "select ACCNUM from IMAGES where _rowid_ =" + rowid;
         try (ResultSet res = query(q)) {
             if (Objects.requireNonNull(res).next()) {
                 System.out.println("readACC: " + rowid);
-                int ret = res.getInt(1);
+                ret = res.getInt(1);
                 // init with 1 on first use
                 if (ret == 0) {
                     setAccCounter(rowid, 1);
                     return 1;
                 }
+                cache.put(rowid, ret);
                 return ret;
             }
         } catch (SQLException e) {
