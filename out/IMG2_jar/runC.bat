f1420ea44a8a48ed34930ffe5d01541ef8e00a52
Administrator <pbull809@gmail.com>
2025-06-29 10:16:06 +0200

    better keyboard handling

---
+++ out/IMG2_jar/runC.bat
@@ -0,0 +1,1 @@
+"C:\Users\Administrator\.jdks\openjdk-23.0.1\bin\java.exe" -Xmx6g -Dfile.encoding=windows-1252 -Dsun.stdout.encoding=windows-1252 -Dsun.stderr.encoding=windows-1252 -jar C:\peter.home\java\IMG3\out\IMG2_jar\IMG2.jar "dbdir:C:\Databases_Copy\Databases\\" &
