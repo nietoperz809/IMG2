2d5cfedcab664a0c02a4f472bb50a8ebfec5a5d5
Administrator <pbull809@gmail.com>
2025-08-23 13:51:18 +0200

    new TimedMsgBox

---
+++ src/dialogs/TimedMsg2.java
@@ -0,0 +1,83 @@
+package dialogs;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.TimerTask;
+
+@FunctionalInterface
+interface TimeoutCallback {
+    void onResult(boolean result);
+}
+
+public class TimedMsg2 extends JDialog {
+    private int thisSec;
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JLabel label;
+    //private final TimeoutCallback thisCB;
+    private boolean retval;
+
+    public TimedMsg2 (String head, /*TimeoutCallback cb,*/ int seconds) {
+        //thisCB = cb;
+        thisSec = seconds;
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+        setTitle(head);
+
+        // timer
+        java.util.Timer t = new java.util.Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                thisSec--;
+                label.setText(""+thisSec);
+                if (thisSec == 0) {
+                    onCancel();
+                }
+            }
+        }, 1000, 1000);
+
+        buttonOK.addActionListener(e -> onOK());
+        buttonCancel.addActionListener(e -> onCancel());
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        retval = true;
+        //thisCB.onResult(true);
+        dispose();
+    }
+
+    private void onCancel() {
+        retval = false;
+        //thisCB.onResult(false);
+        dispose();
+    }
+
+    public static boolean doTimedBox() {
+        TimedMsg2 dialog = new TimedMsg2 (
+                "Shutdown the app",
+                //System.out::println,
+                30);
+        dialog.pack();
+        dialog.setVisible(true);
+        return dialog.retval;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doTimedBox());
+        System.exit(0);
+    }
+}
