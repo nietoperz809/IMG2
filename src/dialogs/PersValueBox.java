50e920b4651276f0f01769b60874bf612e62f88c
Administrator <pbull809@gmail.com>
2025-10-09 08:36:42 +0200

    added reset button in PersValueBox

--- src/dialogs/PersValueBox.java
+++ src/dialogs/PersValueBox.java
@@ -23,6 +23,12 @@ public class PersValueBox extends JDialog {
     public PersValueBox(ImageView imv) {
         setLayout(null);
         setTitle("Perspective Params");
+        setResizable(false);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setModal(true);
+
         ftext_ulx.setToolTipText("Upper Left X");
         ftext_uly.setToolTipText("Upper Left Y");
         ftext_urx.setToolTipText("Upper Right X");
@@ -31,16 +37,6 @@ public class PersValueBox extends JDialog {
         ftext_lry.setToolTipText("Lower Right Y");
         ftext_llx.setToolTipText("Lower Left X");
         ftext_lly.setToolTipText("Lower Left Y");
-
-        ftext_ulx.setText("0");
-        ftext_uly.setText("0");
-        ftext_urx.setText(""+imv.getImage().getWidth());
-        ftext_ury.setText("0");
-
-        ftext_lrx.setText(""+imv.getImage().getWidth());
-        ftext_lry.setText(""+imv.getImage().getHeight());
-        ftext_llx.setText("0");
-        ftext_lly.setText(""+imv.getImage().getHeight());
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -53,59 +49,71 @@ public class PersValueBox extends JDialog {
         JLabel lblNewLabel = new JLabel("ulx");
         lblNewLabel.setBounds(10, 10, 55, 15);
         ftext_ulx.setBounds(31, 10, 76, 21);
-        add (lblNewLabel);
-        add (ftext_ulx);
+        add(lblNewLabel);
+        add(ftext_ulx);
 
         JLabel label = new JLabel("uly");
         label.setBounds(10, 38, 55, 15);
         ftext_uly.setBounds(31, 38, 76, 21);
-        add (label);
-        add (ftext_uly);
+        add(label);
+        add(ftext_uly);
 
         JLabel label_1 = new JLabel("urx");
         label_1.setBounds(10, 65, 55, 15);
         ftext_urx.setBounds(31, 65, 76, 21);
-        add (label_1);
-        add (ftext_urx);
+        add(label_1);
+        add(ftext_urx);
 
         JLabel label_2 = new JLabel("ury");
         label_2.setBounds(10, 95, 55, 15);
         ftext_ury.setBounds(31, 95, 76, 21);
-        add (label_2);
-        add (ftext_ury);
+        add(label_2);
+        add(ftext_ury);
 
         JLabel label_3 = new JLabel("lrx");
         label_3.setBounds(163, 10, 55, 15);
         ftext_lrx.setBounds(184, 10, 76, 21);
-        add (label_3);
-        add (ftext_lrx);
+        add(label_3);
+        add(ftext_lrx);
 
         JLabel label_4 = new JLabel("lry");
         label_4.setBounds(163, 38, 55, 15);
         ftext_lry.setBounds(184, 38, 76, 21);
-        add (label_4);
-        add (ftext_lry);
+        add(label_4);
+        add(ftext_lry);
 
         JLabel label_5 = new JLabel("llx");
         label_5.setBounds(163, 65, 55, 15);
         ftext_llx.setBounds(184, 65, 76, 21);
-        add (label_5);
-        add (ftext_llx);
+        add(label_5);
+        add(ftext_llx);
 
         JLabel label_6 = new JLabel("lly");
         label_6.setBounds(163, 95, 55, 15);
         ftext_lly.setBounds(184, 95, 76, 21);
-        add (label_6);
-        add (ftext_lly);
+        add(label_6);
+        add(ftext_lly);
 
-        setSize (300, 170);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        setModal(true);
+        JButton bt1 = new JButton("Reset");
+        bt1.setToolTipText("Image corner points");
+        bt1.setBounds(184, 125, 76, 21);
+        add(bt1);
+        bt1.addActionListener(_ -> {
+            ftext_ulx.setText("0");
+            ftext_uly.setText("0");
+            ftext_urx.setText("" + imv.getImage().getWidth());
+            ftext_ury.setText("0");
+
+            ftext_lrx.setText("" + imv.getImage().getWidth());
+            ftext_lry.setText("" + imv.getImage().getHeight());
+            ftext_llx.setText("0");
+            ftext_lly.setText("" + imv.getImage().getHeight());
+        });
+
         setVisible(true);
     }
 
-    static double parse (JTextField tf) {
+    static double parse(JTextField tf) {
         String s = tf.getText();
         try {
             return Double.parseDouble(s);
@@ -114,8 +122,8 @@ public class PersValueBox extends JDialog {
         }
     }
 
-    public record PersV (double ulx, double uly, double urx, double ury,
-                         double lrx, double lry, double llx, double lly) {
+    public record PersV(double ulx, double uly, double urx, double ury,
+                        double lrx, double lry, double llx, double lly) {
         public static PersV getValues() {
             return new PersV(
                     parse(ftext_ulx), parse(ftext_uly), parse(ftext_urx), parse(ftext_ury),
