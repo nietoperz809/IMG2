90a7c80666ffd278d07e70a6eb28d1263f2125e8
Administrator <pbull809@gmail.com>
2025-12-03 12:53:44 +0100

    minimal changes

--- src/buildinfo/BuildInfo2.java
+++ src/buildinfo/BuildInfo2.java
@@ -2,8 +2,8 @@
 package buildinfo;
 
 public class BuildInfo2 {
-    public static final String BUILD_NUMBER = "8";
-    public static final String BUILD_DATE = "12/03/2025 at 02:45 AM";
-    public static final String GIT_REV = "387";
+    public static final String BUILD_NUMBER = "9";
+    public static final String BUILD_DATE = "12/03/2025 at 12:53 PM";
+    public static final String GIT_REV = "389";
 }
 
