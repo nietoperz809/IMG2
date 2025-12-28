31feea657d84ae364af1d513e443f1dd3adaac24
Administrator <pbull809@gmail.com>
2025-12-28 02:44:21 +0100

    AnimPlayerBox improved image size

--- src/buildinfo/BuildInfo2.java
+++ src/buildinfo/BuildInfo2.java
@@ -2,8 +2,8 @@
 package buildinfo;
 
 public class BuildInfo2 {
-    public static final String BUILD_NUMBER = "19";
-    public static final String BUILD_DATE = "12/24/2025 at 11:49 PM";
-    public static final String GIT_REV = "418";
+    public static final String BUILD_NUMBER = "20";
+    public static final String BUILD_DATE = "12/28/2025 at 02:44 AM";
+    public static final String GIT_REV = "420";
 }
 
